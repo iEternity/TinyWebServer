f7251581fdc6b650287bb5218ff200ae52589e21
zhangkuo <vczk@foxmail.com>
2017-08-23 21:57:43 +0800

    add Timer

--- net/Timer.cpp
+++ net/Timer.cpp
@@ -1,4 +1,19 @@
 //
 // Created by zhangkuo on 17-8-23.
 //
+#include "Timer.h"
+using namespace WebServer;
 
+AtomicInt64 Timer::s_numCreated_;
+
+void Timer::restart(Timestamp now)
+{
+    if (repeat_)
+    {
+        expiration_ = addTime(now, interval_);
+    }
+    else
+    {
+        expiration_ = Timestamp::invalid();
+    }
+}
