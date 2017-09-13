0ab99d5d522927427b46851a42dc14471eb1edb1
zhangkuo <vczk@foxmail.com>
2017-09-13 23:13:53 +0800

    add EventLoopThread

--- net/EventLoopThread.cpp
+++ net/EventLoopThread.cpp
@@ -1,4 +1,57 @@
 //
 // Created by zhangkuo on 17-9-13.
 //
+#include "EventLoopThread.h"
+#include "EventLoop.h"
 
+using namespace WebServer;
+
+EventLoopThread::EventLoopThread(const ThreadInitCallback& cb,
+                                 const std::string& name):
+        loop_(nullptr),
+        thread_(std::bind(&EventLoopThread::threadFunc, this), name),
+        mutex_(),
+        cond_(mutex_),
+        callback_(cb)
+
+{
+
+}
+
+EventLoopThread::~EventLoopThread()
+{
+    if(loop_ != nullptr)
+    {
+        loop_->quit();
+        thread_.join();
+    }
+}
+
+EventLoop* EventLoopThread::startLoop()
+{
+    thread_.start();
+
+    {
+        MutexLockGuard lock(mutex_);
+        while(loop_ == nullptr)
+        {
+            cond_.wait();
+        }
+    }
+
+    return loop_;
+}
+
+void EventLoopThread::threadFunc()
+{
+    EventLoop loop;
+    if(callback_) callback_(&loop);
+
+    {
+        MutexLockGuard lock(mutex_);
+        loop_ = &loop;
+        cond_.notify();
+    }
+
+    loop_ = nullptr;
+}
--- net/EventLoopThread.h
+++ net/EventLoopThread.h
@@ -5,4 +5,36 @@
 #ifndef WEBSERVER_EVENTLOOPTHREAD_H
 #define WEBSERVER_EVENTLOOPTHREAD_H
 
+#include <boost/noncopyable.hpp>
+#include "../base/Thread.h"
+#include "../base/Mutex.h"
+#include "../base/Condition.h"
+
+namespace WebServer
+{
+
+class EventLoop;
+
+class EventLoopThread : boost::noncopyable
+{
+public:
+    using ThreadInitCallback = std::function<void(EventLoop*)>;
+
+    EventLoopThread(const ThreadInitCallback& cb = ThreadInitCallback(),
+                    const std::string& name = std::string());
+    ~EventLoopThread();
+
+    EventLoop* startLoop();
+private:
+    void threadFunc();
+
+    EventLoop* loop_;
+    Thread thread_;
+    MutexLock mutex_;
+    Condition cond_;
+    ThreadInitCallback callback_;
+};
+
+}
+
 #endif //WEBSERVER_EVENTLOOPTHREAD_H
