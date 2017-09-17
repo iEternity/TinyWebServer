6af39e78cfe91c5a3974bb6fac423cddaa68011a
zhangkuo <vczk@foxmail.com>
2017-09-17 20:31:19 +0800

    update TcpServer

--- net/TcpServer.cpp
+++ net/TcpServer.cpp
@@ -2,3 +2,23 @@
 // Created by zhangkuo on 17-9-14.
 //
 
+#include "TcpServer.h"
+#include "EventLoop.h"
+#include "Acceptor.h"
+#include "EventLoopThreadPool.h"
+
+using namespace WebServer;
+
+TcpServer::TcpServer(EventLoop* loop,
+                     const InetAddress& listenAddr,
+                     const std::string& nameArg,
+                     Option option):
+        loop_(loop),
+        ipPort_(listenAddr.toIpPort()),
+        name_(nameArg),
+        acceptor_(new Acceptor(loop_, listenAddr, option == kReusePort)),
+        threadPool_(new EventLoopThreadPool(loop, nameArg))
+
+{
+
+}
--- net/TcpServer.h
+++ net/TcpServer.h
@@ -7,11 +7,17 @@
 #include <boost/noncopyable.hpp>
 #include <functional>
 #include "InetAddress.h"
+#include "Callbacks.h"
+#include <boost/scoped_ptr.hpp>
+#include <map>
+#include "../base/Atomic.h"
 
 namespace WebServer
 {
 
 class EventLoop;
+class Acceptor;
+class EventLoopThreadPool;
 
 class TcpServer : boost::noncopyable
 {
@@ -39,14 +45,26 @@ public:
     void setThreadInitCallback(ThreadInitCallback&& cb)
     { threadInitCallback_ = std::move(cb); }
 
-
+    std::shared_ptr<EventLoopThreadPool> getThreadPool()
+    {
+        return threadPool_;
+    }
 
 private:
+    using ConnectionMap = std::map<std::string, TcpConnectionPtr >;
     EventLoop* loop_;
     const std::string ipPort_;
     const std::string name_;
-
+    boost::scoped_ptr<Acceptor> acceptor_;
+    std::shared_ptr<EventLoopThreadPool> threadPool_;
+    ConnectionCallback connectionCallback_;
+    MessageCallback messageCallback_;
+    WriteCompleteCallback writeCompleteCallback_;
     ThreadInitCallback threadInitCallback_;
+
+    AtomicInt32 started_;
+    int nextConnId_;
+    ConnectionMap connections_;
 };
 
 }
