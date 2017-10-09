e1daff1718608d2d45c48bc7ab8ef13bfc0f028c
zhangkuo <vczk@foxmail.com>
2017-10-09 22:58:21 +0800

    update CMakeLists.txt

---
+++ base/LogStream.h
@@ -0,0 +1,8 @@
+//
+// Created by zhangkuo on 17-9-27.
+//
+
+#ifndef WEBSERVER_LOGSTREAM_H
+#define WEBSERVER_LOGSTREAM_H
+
+#endif //WEBSERVER_LOGSTREAM_H
---
+++ base/Logging.h
@@ -0,0 +1,8 @@
+//
+// Created by zhangkuo on 17-9-27.
+//
+
+#ifndef WEBSERVER_LOGGING_H
+#define WEBSERVER_LOGGING_H
+
+#endif //WEBSERVER_LOGGING_H
